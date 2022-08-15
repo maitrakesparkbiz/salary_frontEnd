40379d7b9033db010626c141f93a161329494529
maitrakesparkbiz <nirmal.maitrak@esparkbizmail.com>
2022-08-15 11:30:24 +0530

    header bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AddExpenseComponent} from './add-expense/add-expense.component';
+import { AppComponent } from './app.component';
 import {CategoryComponent} from './category/category.component';
 import {ExpenseEntryComponent} from './expense-entry/expense-entry.component';
 import {ExpenseListComponent} from './expense-list/expense-list.component';
@@ -8,12 +9,13 @@ import {AuthGuard} from './login/auth.guard';
 import {LoginComponent} from './login/login.component';
 
 const expenseRoute:Routes= [
-
+  { path:'',component:AppComponent},
   {path:"Home" , canActivate:[AuthGuard], component:ExpenseEntryComponent},
   {path:"Report" , canActivate:[AuthGuard], component:ExpenseListComponent},
   {path:"Add" , canActivate:[AuthGuard], component:AddExpenseComponent},
   {path:"Report/:id" , canActivate:[AuthGuard], component:AddExpenseComponent},
   {path:"Category" , canActivate:[AuthGuard], component:CategoryComponent},
+
   // {path:"Login" , component:LoginComponent},
   // {path:"Logout" , component:LoginComponent},
 
